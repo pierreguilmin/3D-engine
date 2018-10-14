53a3ea87e70c960cd6d01b9332a6803ab825bb0f
Pierre Guilmin <pierre.guilmin@mines-paristech.fr>
2018-10-14 17:59:54 -0400

    minor modifications

--- src/utils/mouse.cpp
+++ src/utils/mouse.cpp
@@ -4,20 +4,20 @@ unsigned Mouse::last_position_x = 0;
 unsigned Mouse::last_position_y = 0;
 
 int Mouse::get_move_x(const sf::RenderWindow &window) {
-	int move_x = getPosition(window).x - last_position_x;
+	const int move_x = getPosition(window).x - last_position_x;
 	last_position_x = getPosition(window).x;
 
 	return move_x;
 }
 
 int Mouse::get_move_y(const sf::RenderWindow &window) {
-	int move_y = getPosition(window).y - last_position_y;
+	const int move_y = getPosition(window).y - last_position_y;
 	last_position_y = getPosition(window).y;
 
 	return move_y;
 }
 
-// extend sf::Mouse inherited function
+// extend sf::Mouse setPosition function
 void Mouse::setPosition(const sf::Vector2i &position, const sf::Window &relativeTo) {
 	sf::Mouse::setPosition(position, relativeTo);
 
