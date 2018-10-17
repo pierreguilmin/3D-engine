bde2ef40ab425fc82e845610a868bdfe886a966d
Pierre Guilmin <pierre.guilmin@mines-paristech.fr>
2018-10-17 19:45:24 -0400

    typos and style

--- src/geometry/segment3d.cpp
+++ src/geometry/segment3d.cpp
@@ -1,5 +1,9 @@
 #include "segment3d.hpp"
 
+// ##############################################
+// ## operators  ################################
+// ##############################################
+
 Segment3d& Segment3d::operator=(const Segment3d &s) {
     if (this != &s) {
         a = s.a;
